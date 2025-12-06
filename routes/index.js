18c739f1b0f93adeab21cec7070e9eddf1347451
Southopho <southopho@pm.me>
2025-12-06 13:47:26 +0200

    This is a joke

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Express" });
+    // log url variables
+    console.log(req.query);
+});
+
+module.exports = router;
